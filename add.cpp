31aa3e76e590fa132938eb45bf9150fead834396
Asif Ahmed <asifahmed331999@gmail.com>
2023-10-16 00:34:12 +0530

    program

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a,b,sum;
+    cout<<"enter two number a and b."<<endl;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"Addtition of two number is="<<sum;
+    return 0;
+
+}
