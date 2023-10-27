bf26cd407d9f629a50b3c15deab0c48375461f3d
Asif Ahmed <asifahmed331999@gmail.com>
2023-10-27 15:33:54 +0530

    add new lines

--- vector.cpp
+++ vector.cpp
@@ -6,11 +6,16 @@ int main(){
     //vector<int> *v=new vector<int>();
 
     vector<int> v;
-    v.push_back(10);
-v.push_back(30);
-v.push_back(60);
-cout<<v.size()<<endl;
-cout<<v.at(1);
+    v.push_back(20);
+    v.push_back(60);
+    v.push_back(65);
+    v.push_back(90);
+    for(int i=0;i<v.size();i++){
+    cout<<v.at(i);
+
+    cout<<endl;
+    }
+
 
 
 
