31a9dd1a116c6a7c4c8fbe4af7860cc576361497
Asif Ahmed <asifahmed331999@gmail.com>
2023-11-08 21:58:49 +0530

    add new things

--- pointer.cpp
+++ pointer.cpp
@@ -1,23 +1,11 @@
 #include<iostream>
 using namespace std;
 
-void fun( int ** p){
-     cout<<(**p);
-    
-}
-
 int main(){
-    int a=10;
-    void *ptr=0;   //All in one pointer its contain the address of all pointer
-     //but during dereferance type casting is needed.
-    ptr=&a;
-    char ch='a';
-    ptr=&ch;
-    
-    cout<<*(char *)ptr<<endl;
-
-    cout<<ptr;
-
-    return 0;
+    int i=65;
+    char ch=i;
+    int *p=&i;
+    char*ptr=(char*)p;
+    cout<<*ptr;
 
 }
